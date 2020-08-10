d3f2ddf7927602db448e9d65939862f1e196e79b
Zach <awdx8326@gmail.com>
2020-08-10 22:33:14 +0800

    set web address in index.js and reset App.vue

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,63 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '*',
+    redirect: '/'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/admin',
+    redirect: '/admin/products'
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    children:
+    [
+      {
+        path: '/about',
+        component: () => import('../views/About.vue')
+      },
+      {
+        path: '/products',
+        component: () => import('../views/Products.vue')
+      },
+      {
+        path: '/product/:id',
+        component: () => import('../views/Product.vue')
+      },
+      {
+        path: '/login',
+        component: () => import('../views/Login.vue')
+      },
+      {
+        path: '/cart',
+        component: () => import('../views/Cart.vue')
+      }
+    ]
+  },
+  {
+    path: '/admin',
+    component: () => import('../views/Dashboard'),
+    children:
+    [
+      {
+        path: 'coupons',
+        component: () => import('../views/admin/Coupons.vue')
+      },
+      {
+        path: 'orders',
+        component: () => import('../views/admin/Orders.vue')
+      },
+      {
+        path: 'products',
+        component: () => import('../views/admin/Products.vue')
+      },
+      {
+        path: 'storage',
+        component: () => import('../views/admin/Storage.vue')
+      }
+    ]
   }
 ]
 
