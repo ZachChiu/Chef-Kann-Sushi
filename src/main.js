8cada818ff5fc424ccce094a042bbd8125c55fe5
Zach <awdx8326@gmail.com>
2020-08-10 21:46:45 +0800

    npm font-awesome install

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import router from './router'
 
+library.add(faBars)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
