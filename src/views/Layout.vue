3343ff1a36c081b03ef74b676eeb01d85808038e
Zach <awdx8326@gmail.com>
2020-08-10 22:53:30 +0800

    modify Layout.vue

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -4,7 +4,7 @@
       <router-link to="/">首頁</router-link> |
       <router-link to="/about">關於</router-link> |
       <router-link to="/products">產品</router-link> |
-      <router-link to="/products">購物車</router-link> |
+      <router-link to="/cart">購物車</router-link> |
       <router-link to="/login">登入</router-link>
     </div>
     <img alt="Vue logo" src="../assets/logo.png">
