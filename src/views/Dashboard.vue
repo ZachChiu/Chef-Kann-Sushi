ad5bbec6ba35904c0ef8fe172aa2de68cd7af4db
Zach <awdx8326@gmail.com>
2020-08-10 22:50:22 +0800

    add router-link and style in Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,88 @@
 <template>
-  <div>
-    <p>這是後台首頁列表</p>
+  <div class="backstage">
+    <nav>
+      <router-link to="/">返回前台</router-link>
+      <router-link to="/admin" :class="{active:pageLocate == 'products'}" @click="changeLocate()" >產品列表</router-link>
+      <router-link to="/admin/orders" :class="{active:pageLocate == 'orders'}" @click="changeLocate()" >訂單列表</router-link>
+      <router-link to="/admin/coupons" :class="{active:pageLocate == 'coupons'}" @click="changeLocate()" >優惠卷列表</router-link>
+      <router-link to="/admin/storage" :class="{active:pageLocate == 'storage'}" @click="changeLocate()" >檔案列表</router-link>
+      <router-link to="/login">登出</router-link>
+    </nav>
+    <main>
+      <header>
+        <font-awesome-icon class="burger" :icon="['fas', 'bars']"/>
+        <h2>乾師傅壽司</h2>
+      </header>
+      <router-view></router-view>
+    </main>
   </div>
 </template>
+<style lang="scss">
+*{
+  box-sizing: border-box;
+}
+.backstage {
+  nav {
+    position:fixed ;
+    width: 10%;
+    background: rgb(68, 68, 68);
+    height:100vh;
+    a {
+      text-align: center;
+      padding: 20px 0;
+      display: block;
+      color: white;
+      background: rgb(68, 68, 68);
+      &:hover {
+        background: rgb(124, 155, 165);
+      }
+    }
+    .active{
+        background: rgb(192, 238, 253) !important;
+        color: rgb(27, 27, 27);
+    }
+  }
+  main{
+    width: 90%;
+    margin-left:10% ;
+    header{
+      background: rgb(68, 68, 68);
+      color: white;
+      padding: 15px 0;
+      font-size: 26px;
+      font-weight: bold;
+      position: relative;
+    }
+    .burger{
+      display: none;
+      position: absolute;
+      top: 15px;
+      left: 15px;
+    }
+  }
+  .delModal{
+    position: fixed;
+    top:10%;
+    left: 20%;
+  }
+}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      pageLocate: 'products',
+      delModal: '',
+      tempProduct: {}
+    }
+  },
+  methods: {
+    changeLocate: function () {
+      this.pageLocate = 'coupons'
+    },
+    closeModal: function () {
+    }
+  }
+}
+</script>
